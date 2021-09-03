f9fc18e012a8aba1c2411ca0f64145806fc90e23
chushka-web <globnicktime@gmail.com>
2021-09-03 18:46:25 +0300

    create stopwatch

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable,  Subscription} from "rxjs";
+import { timer } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,48 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'stopwatch-angular';
+
+  startTime = new Date().getTime();
+  sub!: Subscription;
+  timer: Observable<number> = timer(this.startTime, 1000);
+
+  hours = 0;
+  seconds = 0;
+  minutes = 0;
+
+  timerActive = false;
+
+  onStart() {
+    if (this.timerActive) {
+      this.sub.unsubscribe();
+      this.timerActive = false;
+      this.hours = 0;
+      this.minutes = 0;
+      this.seconds = 0;
+    }
+    else {
+      this.timerActive = true;
+      this.sub = this.timer
+        .subscribe(number => {
+            this.hours = Math.floor(number / 3600)
+            this.minutes = Math.floor((number % 3600) / 60);
+            this.seconds = Math.floor(number % 60);
+          }
+        );
+    }
+  }
+
+  onWait() {
+    if (this.timerActive) {
+      this.sub.unsubscribe();
+      this.timerActive = false;
+    }
+  }
+
+  onReset() {
+    this.sub.unsubscribe();
+    this.timerActive = false;
+    this.onStart();
+  }
+
 }
